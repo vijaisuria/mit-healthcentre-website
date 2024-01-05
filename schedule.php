aeb79bf310be6680c3ec7f541561eb460af32153
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-01-05 16:56:01 +0530

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -44,19 +44,21 @@
         <div class="container">
             <div class="section-padding">
                 <div class="section-top-border">
-                    <h3 class="mb-30">Table</h3>
+                    <h3 class="mb-30">SPECIALITY CLINICS</h3>
                     <div class="progress-table-wrap">
                         <div class="progress-table">
                             <div class="table-head">
                                 <div class="serial">#</div>
-                                <div class="country">Countries</div>
-                                <div class="visit">Visits</div>
-                                <div class="percentage">Percentages</div>
+                                <div class="country">Speciality</div>
+                                <div class="visit">Day</div>
+                                <div class="phone">Phone</div>
+                                <div class="percentage">Visiting Hours</div>
                             </div>
                             <div class="table-row">
                                 <div class="serial">01</div>
                                 <div class="country"> <img src="assets/img/elements/f1.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-1" role="progressbar" style="width: 80%"
@@ -68,6 +70,7 @@
                                 <div class="serial">02</div>
                                 <div class="country"> <img src="assets/img/elements/f2.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-2" role="progressbar" style="width: 30%"
@@ -79,6 +82,7 @@
                                 <div class="serial">03</div>
                                 <div class="country"> <img src="assets/img/elements/f3.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-3" role="progressbar" style="width: 55%"
@@ -90,6 +94,7 @@
                                 <div class="serial">04</div>
                                 <div class="country"> <img src="assets/img/elements/f4.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-4" role="progressbar" style="width: 60%"
@@ -101,6 +106,7 @@
                                 <div class="serial">05</div>
                                 <div class="country"> <img src="assets/img/elements/f5.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-5" role="progressbar" style="width: 40%"
@@ -112,6 +118,7 @@
                                 <div class="serial">06</div>
                                 <div class="country"> <img src="assets/img/elements/f6.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-6" role="progressbar" style="width: 70%"
@@ -123,6 +130,7 @@
                                 <div class="serial">07</div>
                                 <div class="country"> <img src="assets/img/elements/f7.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-7" role="progressbar" style="width: 30%"
@@ -134,6 +142,118 @@
                                 <div class="serial">08</div>
                                 <div class="country"> <img src="assets/img/elements/f8.jpg" alt="flag">Canada</div>
                                 <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-8" role="progressbar" style="width: 60%"
+                                            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-top-border">
+                    <h3 class="mb-30">CLINICAL LABORATORY</h3>
+                    <div class="progress-table-wrap">
+                        <div class="progress-table">
+                            <div class="table-head">
+                                <div class="serial">#</div>
+                                <div class="country">Item</div>
+                                <div class="visit">Day</div>
+                                <div class="phone">Phone</div>
+                                <div class="percentage">Visiting Hours</div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">01</div>
+                                <div class="country"> <img src="assets/img/elements/f1.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-1" role="progressbar" style="width: 80%"
+                                            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">02</div>
+                                <div class="country"> <img src="assets/img/elements/f2.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-2" role="progressbar" style="width: 30%"
+                                            aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">03</div>
+                                <div class="country"> <img src="assets/img/elements/f3.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-3" role="progressbar" style="width: 55%"
+                                            aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">04</div>
+                                <div class="country"> <img src="assets/img/elements/f4.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-4" role="progressbar" style="width: 60%"
+                                            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">05</div>
+                                <div class="country"> <img src="assets/img/elements/f5.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-5" role="progressbar" style="width: 40%"
+                                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">06</div>
+                                <div class="country"> <img src="assets/img/elements/f6.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-6" role="progressbar" style="width: 70%"
+                                            aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">07</div>
+                                <div class="country"> <img src="assets/img/elements/f7.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
+                                <div class="percentage">
+                                    <div class="progress">
+                                        <div class="progress-bar color-7" role="progressbar" style="width: 30%"
+                                            aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="serial">08</div>
+                                <div class="country"> <img src="assets/img/elements/f8.jpg" alt="flag">Canada</div>
+                                <div class="visit">645032</div>
+                                <div class="phone">645032</div>
                                 <div class="percentage">
                                     <div class="progress">
                                         <div class="progress-bar color-8" role="progressbar" style="width: 60%"
