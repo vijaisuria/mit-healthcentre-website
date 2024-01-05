af4b93aaa027d1e81e2d0bdd4279ac7860829d0e
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-01-05 16:35:31 +0530

    Add preloader and header code

---
+++ includes/footer.php
@@ -0,0 +1,92 @@
+<footer>
+    <div class="footer-area section-bg">
+        <div class="container">
+            <div class="footer-top footer-padding">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-5">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>More Links</h4>
+                                <ul>
+                                    <li><a href="index.php" target="_blank">Home</a></li>
+                                    <li><a href="event.html" target="_blank">Events</a></li>
+                                    <li><a href="gallery.html" target="_blank">Gallery</a></li>
+                                    <li><a href="feedback.php" target="_blank">Feedback</a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-5">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>Quick Links</h4>
+                                <div class="footer-pera">
+                                    <ul>
+                                        <li><a href="https://annauniv.edu/" target="_blank">Anna University</a></li>
+                                        <li><a href="https://www.mitindia.edu/en/" target="_blank">MIT</a></li>
+                                        <li><a href="http://www.health-centre.mitindia.edu/healthCare/" target="_blank">
+                                                Automation Software</a></li>
+                                        <li><a href="https://www.auegov.ac.in/" target="_blank">Centre for
+                                                e-Governance <span>(C-eGov)</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-8">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-number mb-50">
+                                <h4><span>044 </span>2251 6193</h4>
+                                <p>healthcentremit@gmail.com</p>
+                                <h4 style="font-weight: 900; font-size: 2.5rem;">24/7
+                                    Ambulance:
+                                    <span style="color: aliceblue;">9498419967</span>
+                                </h4>
+                            </div>
+                            <div class="footer-tittle">
+                                <h4>Address</h4>
+                            </div>
+                            <div class="footer-pera">
+                                <p class="info1">Dr. V. P Jayachitra <br>
+                                    Deputy Director, Health Centre,
+                                    Madras Institute of Technology Campus,Anna University
+                                    Chromepet, Chennai,
+                                    Tamil Nadu 600044,
+                                    India.
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-xl-9 col-lg-8">
+                        <div class="footer-copy-right">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;
+                                2023 All rights reserved |
+                                Developed with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="team.html"
+                                    target="_blank">MIT Web Team</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-4">
+                        <!-- Footer Social -->
+                        <div class="footer-social f-right">
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fas fa-globe"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,71 @@
+<header>
+    <!--? Header Start -->
+    <div class="header-area">
+        <div class="main-header header-sticky">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <!-- Logo -->
+                    <div class="col-xl-2 col-lg-1 col-md-1">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/img/logo/au.png" class="desktop-logo" alt=""></a>
+                            <!-- Desktop icon-->
+                            <a href="index.html"><img src="assets/img/logo/mit-hc-logo-tab.png" class="au-logo"
+                                    alt=""></a> <!-- Mobile icon-->
+                            <a href="index.html"><img src="assets/img/logo/au.png" class="tablet-logo" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-10 col-md-10">
+                        <div class="container">
+                            <div class="nav-title">
+                                <h1>HEALTH CENTRE <br> MADRAS INSTITUTE OF TECHNOLOGY</h1>
+                            </div>
+                        </div>
+                        <div class="menu-main d-flex align-items-center justify-content-center">
+                            <!-- Main-menu -->
+                            <div class="main-menu f-right d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php">About</a></li>
+                                        <li><a href="staff.php">Staffs</a></li>
+                                        <li><a href="#">Services</a>
+                                            <ul class="submenu">
+                                                <li><a href="facility.php">Facilities</a></li>
+                                                <li><a href="equipments.php">Eqiupments</a></li>
+                                                <li><a href="schedule.php">Doctor Schedules</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="blog.html">Blog</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Gallery</a></li>
+                                                <li><a href="blog_details.html">Recent Events</a></li>
+                                                <li><a href="blog.html">Magazines</a></li>
+                                                <li><a href="blog.html">Blog</a></li>
+                                                <li><a href="blog_details.html">Blog Details</a></li>
+                                                <li><a href="elements.html">Element</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.php">Contact</a></li>
+                                        <li><a href="#" class="btn header-btn text-white">HealthCare+</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-1 col-md-1">
+                        <div class="logo" style="float: right;">
+                            <a href="index.html"><img src="assets/img/logo/mit-logo.png" class="desktop-logo"
+                                    alt=""></a>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+</header>
